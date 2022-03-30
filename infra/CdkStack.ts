a10784ee42d0e12c31a477fde5487b169db49534
Bobby Wang <bobbyquennell@gmail.com>
2022-03-30 15:48:43 +1100

    restructure the CDK files into infra folder

--- infra/CdkStack.ts
+++ infra/CdkStack.ts
@@ -10,14 +10,18 @@ export class CdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const notificationHandler = new NodejsFunction(this, 'NotificationHandler', {
-      functionName: 'notificationHandler',
-      description: 'Get notifications from DynamoDB',
-      runtime: lambda.Runtime.NODEJS_14_X,
-      architecture: lambda.Architecture.ARM_64,
-      entry: path.join(__dirname, '../src/lambda/notification-handler.ts'),
-      handler: 'notificationHandler',
-    });
+    const notificationHandler = new NodejsFunction(
+      this,
+      'NotificationHandler',
+      {
+        functionName: 'notificationHandler',
+        description: 'Get notifications from DynamoDB',
+        runtime: lambda.Runtime.NODEJS_14_X,
+        architecture: lambda.Architecture.ARM_64,
+        entry: path.join(__dirname, '../src/lambda/notification-handler.ts'),
+        handler: 'notificationHandler',
+      },
+    );
 
     new apigateway.LambdaRestApi(this, 'Endpoint', {
       handler: notificationHandler,
